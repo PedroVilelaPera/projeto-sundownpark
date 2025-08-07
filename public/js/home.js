95b824c4d6b81e5bf7fa62e1ddf9dd6e468bc819
Álvaro Kayc <155195361+alvarokaycss@users.noreply.github.com>
2025-08-06 23:00:09 -0300

    add class img

--- public/js/home.js
+++ public/js/home.js
@@ -10,7 +10,7 @@ function criarCardIngresso(ingresso) {
 
     return `
         <div class="produto">
-            <img src="${ingresso.caminho_img}" alt="ingresso" style="width:100%; height: 67%;">
+            <img src="${ingresso.caminho_img}" alt="ingresso" class="formatar-img-ingressos-home">
                 <div class="texto-produto">
                     <h2>${ingresso.nome}</h2>
                     <a href="/ingressos" class="saiba-mais">Saiba Mais</a>
